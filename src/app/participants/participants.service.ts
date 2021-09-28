c22df18ea506ade330bca72102d2fa5c83892885
hannako <nhaj.gwaps@gmail.com>
2021-09-28 23:55:00 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,13 @@
+export class ParticapantsService {
+  getParticipants(){
+      return [
+          {name:"Hanna Jane Blantucas", age:"21", gender:"Female"},
+          {name:"Athena Maries", age:"21", gender:"Female"},
+          {name:"Alvin Jose Orcullo", age:"21", gender:"Male"},
+          {name:"Jethro Cris Ladeza", age:"22", gender:"Male"},
+          {name:"Cyreen Cabahug", age:"21", gender:"Female"},
+          {name:"Clear Joy Petalino", age:"22", gender:"Female"},
+          {name:"Angel Jabal", age:"21", gender:"Female"},
+        ];
+  }
+}
